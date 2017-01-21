d61761609cd65395b078a7992dc9d8e062e52b56
Alex Ou <aou4@ucsc.edu>
2017-01-21 15:57:44 -0800

    added client

---
+++ iOSClient/imagepicker/TakePictureViewController.swift
@@ -0,0 +1,101 @@
+//
+//  TakePictureViewController.swift
+//  imagepicker
+//
+//  Created by Alexander Ou on 1/21/17.
+//  Copyright © 2017 Sara Robinson. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class TakePictureViewController: UIViewController, UINavigationControllerDelegate {
+    
+    var captureSession : AVCaptureSession?
+    var stillImageOutput: AVCaptureStillImageOutput?
+    var previewLayer : AVCaptureVideoPreviewLayer?
+
+    @IBOutlet var cameraView: UIView!
+    
+    @IBOutlet weak var takePicOutlet: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib
+        
+    }
+//    
+//    override var prefersStatusBarHidden: Bool {
+//        return true
+//    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+        
+        captureSession = AVCaptureSession()
+        captureSession!.sessionPreset = AVCaptureSessionPresetPhoto
+        
+        let backCamera = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        var error: NSError?
+        var input: AVCaptureDeviceInput!
+        do {
+            input = try AVCaptureDeviceInput(device: backCamera)
+        } catch let error1 as NSError {
+            error = error1
+            input = nil
+        }
+        
+        //self.cameraView.frame = CGRect(x: 0, y: 0, width: 375, height: 667)
+        cameraView.frame = self.view.frame
+        
+        if error == nil && captureSession!.canAddInput(input) {
+            captureSession!.addInput(input)
+            
+            stillImageOutput = AVCaptureStillImageOutput()
+            stillImageOutput!.outputSettings = [AVVideoCodecKey: AVVideoCodecJPEG]
+            if captureSession!.canAddOutput(stillImageOutput) {
+                captureSession!.addOutput(stillImageOutput)
+                
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                previewLayer!.videoGravity = AVLayerVideoGravityResizeAspectFill
+                previewLayer!.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+                cameraView.layer.addSublayer(previewLayer!)
+                
+                captureSession!.startRunning()
+            }
+        }
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer!.frame = cameraView.bounds
+        self.view.bringSubview(toFront: takePicOutlet)
+
+    }
+    
+    @IBAction func takePic(_ sender: Any) {
+        if let videoConnection = stillImageOutput!.connection(withMediaType: AVMediaTypeVideo) {
+            videoConnection.videoOrientation = AVCaptureVideoOrientation.portrait
+            stillImageOutput?.captureStillImageAsynchronously(from: videoConnection, completionHandler: {(sampleBuffer, error) in
+                if (sampleBuffer != nil) {
+                    let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
+                    let dataProvider = CGDataProvider(data: imageData as! CFData)
+                    let cgImageRef = CGImage(jpegDataProviderSource: dataProvider!, decode: nil, shouldInterpolate: true, intent: CGColorRenderingIntent.defaultIntent)
+                    
+                    //WE NEED TO SEND image to server
+                    let image = UIImage(cgImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.right)
+                    //self.capturedImage.image = image
+                    print("took picture")
+                }
+            })
+        }
+    }
+    
+   }
